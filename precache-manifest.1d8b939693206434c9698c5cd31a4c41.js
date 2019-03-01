7e8986c334666e245812baceba51caa799e8c654
Valentyn <valantine10010@gmail.com>
2019-03-01 16:12:58 +0200

    Updates

---
+++ precache-manifest.1d8b939693206434c9698c5cd31a4c41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6c5e42285ab51a52e445",
+    "url": "/static/js/main.6c5e4228.chunk.js"
+  },
+  {
+    "revision": "7478ee22095f4bf53083",
+    "url": "/static/js/2.7478ee22.chunk.js"
+  },
+  {
+    "revision": "6c5e42285ab51a52e445",
+    "url": "/static/css/main.e1637fa3.chunk.css"
+  },
+  {
+    "revision": "d2cb0ee511f02763c6873e5c9e155a8e",
+    "url": "/index.html"
+  }
+];
